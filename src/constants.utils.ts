16131c9eef5c639d49db2eb767806aec6b6da4ba
Mert1s <Andgtsc@Gmail.com>
2022-04-12 20:29:38 -0300

    Login finalizado

--- src/constants.utils.ts
+++ src/constants.utils.ts
@@ -1,9 +1,27 @@
+import { DocumentBuilder } from "@nestjs/swagger";
 import { dirname, join } from "path"
 
 const currentDir = dirname('./')
 export default {
     rota_raiz: currentDir,
-    rota_upload: join(currentDir, 'src/upload/uploaded_file'),    
+    rota_upload: join(currentDir, 'src/upload/uploaded_file'),
     rota_extraido: join(currentDir, 'src/upload/uncompressed_files'),
-    rota_arquivo_extraido: (nomeArquivo: string):string => join(currentDir, 'src/upload/uncompressed_files',nomeArquivo)
+    rota_arquivo_extraido: (nomeArquivo: string): string => join(currentDir, 'src/upload/uncompressed_files', nomeArquivo),
+    cors: {
+        options: {
+            origin: ['https://leitorxml.herokuapp.com', 'http://leitorxml.herokuapp.com', 'leitorxml.herokuapp.com', 'https://leitorxml.herokuapp.com/upload', 'http://leitorxml.herokuapp.com/upload', 'leitorxml.herokuapp.com/upload', 'http://localhost:3000', 'https://localhost:3000'],
+            methods: ['GET', 'PUT', 'POST'],
+            preflightContinue: false
+        }
+    },
+    swagger: {
+        get config() {
+            return new DocumentBuilder()
+                .setTitle('Leitor XML')
+                .setDescription('...')
+                .setVersion('1.0')
+                .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT', name: 'JWT', in: 'header' }, 'access-token') // Definir para para quem for usar a anotação @ApiBearerAuth('access-token') no controller. Fonte: https://stackoverflow.com/questions/54802832/is-it-possible-to-add-authentication-to-access-to-nestjs-swagger-explorer
+                .build();
+        }
     }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import constantsUtils from './constants.utils';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: ['https://leitorxml.herokuapp.com', 'http://leitorxml.herokuapp.com', 'leitorxml.herokuapp.com', 'https://leitorxml.herokuapp.com/upload', 'http://leitorxml.herokuapp.com/upload', 'leitorxml.herokuapp.com/upload', 'http://localhost:3000'],
-    methods: ['GET', 'PUT', 'POST'],
-    preflightContinue: false
-  }
-  app.enableCors(options)
-  app.useGlobalPipes(new ValidationPipe())
-  const config = new DocumentBuilder()
-    .setTitle('Leitor XML')
-    .setDescription('...')
-    .setVersion('1.0')
-    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT', name: 'JWT', in: 'header' }, 'access-token') // Definir para para quem for usar a anotação @ApiBearerAuth('access-token') no controller. Fonte: https://stackoverflow.com/questions/54802832/is-it-possible-to-add-authentication-to-access-to-nestjs-swagger-explorer
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);  
+  const {cors , swagger } = constantsUtils
   const port = process.env.PORT || 8080
+  const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, swagger.config);
+  
+  app.enableCors(cors.options)
+  app.useGlobalPipes(new ValidationPipe()); 
+  
+  SwaggerModule.setup('api', app, document);  
+  
   await app.listen(port, () => {
     console.log('Servidor iniciado na porta: ' + port)
   })
