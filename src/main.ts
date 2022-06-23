e0b70630c139f3236a810df553a66469e4594c38
Anderson da Silva Costa <Andgtsc@Gmail.com>
2022-06-23 13:31:16 -0300

    Desativando HTTPS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const { rota_extraido, rota_upload } = constantsUtils
 async function bootstrap() {
   const { cors, swagger, httpsOptions } = constantsUtils
   const port = process.env.PORT || 8080
-  const app = await NestFactory.create(AppModule, {httpsOptions});
+  const app = await NestFactory.create(AppModule/*, {httpsOptions}*/);
   const document = SwaggerModule.createDocument(app, swagger.config);
 
   app.enableCors(cors.options)
